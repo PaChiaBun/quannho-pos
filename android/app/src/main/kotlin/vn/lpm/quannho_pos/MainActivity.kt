a7ca72996aa6ca745ef87c13938a22f7e7fe5d46
PaChiaBun <89531632+PaChiaBun@users.noreply.github.com>
2026-06-21 18:00:19 +0700

    fix(build): resolve remaining git merge conflict markers in android and windows platforms

--- android/app/src/main/kotlin/vn/lpm/quannho_pos/MainActivity.kt
+++ android/app/src/main/kotlin/vn/lpm/quannho_pos/MainActivity.kt
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package vn.lpm.quannho_pos
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
-=======
-package vn.lpm.quannho_pos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
->>>>>>> 4bec718df870807743eeb9abb9ea162ca4d749df
